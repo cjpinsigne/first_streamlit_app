76fb308d87de79d19c968691e1d67bdef59fa73e
cjpinsigne <155599717+cjpinsigne@users.noreply.github.com>
2024-01-04 12:00:49 +0800

    Update streamlit_app.py
    fixed error on FTShow

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Lets put selection here
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
