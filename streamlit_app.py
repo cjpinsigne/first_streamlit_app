01a5cfe0517cc14261f50b0e45f20c94e9fc72f9
cjpinsigne <155599717+cjpinsigne@users.noreply.github.com>
2024-01-08 15:38:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urlib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('🥗 heeellloooo')
 streamlit.header('🐔 This is a sample header')
