89a39a62d884a864cd5fcb6692637e4684d3ad49
cjpinsigne <155599717+cjpinsigne@users.noreply.github.com>
2024-01-11 12:03:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if streamlit.button('Add a Fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
 streamlit.stop()
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
