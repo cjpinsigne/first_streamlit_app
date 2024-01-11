73409219620c09e1f0733d5ab9165afef47a0aa9
cjpinsigne <155599717+cjpinsigne@users.noreply.github.com>
2024-01-11 10:12:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,13 +34,13 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()
+#except URLError as e:
+ #streamlit.error()
 #streamlit.write('The user entered ', fruit_choice)
 
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 streamlit.stop()
 
