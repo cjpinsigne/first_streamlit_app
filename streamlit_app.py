7add915308ccc50e2156f10fc00b71753a8a0f9b
cjpinsigne <155599717+cjpinsigne@users.noreply.github.com>
2024-01-11 11:11:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,9 @@ except URLError as e:
 #streamlit.write('The user entered ', fruit_choice)
 
 streamlit.stop()
-get_fruityvice_data(this_fruit_choice):
+
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+
 
 
 
